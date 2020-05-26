761725613f00691db6b6ec7b853618a1b512f3cb
Yun <rudobco0825@gmail.com>
2020-05-26 15:17:46 +0900

    [ADD] Workmanager class, connect to firebase

---
+++ app/src/main/java/com/waterdiary/drinkreminder/worker/PostureWorker.java
@@ -0,0 +1,33 @@
+package com.waterdiary.drinkreminder.worker;
+
+import android.content.Context;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class PostureWorker extends Worker {
+
+
+    public PostureWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+
+        // Call a function I want to execute.
+
+        return Result.success();
+    }
+
+    // Put classifer
+    private void postureClassfier(){
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+        }
+    }
+}
