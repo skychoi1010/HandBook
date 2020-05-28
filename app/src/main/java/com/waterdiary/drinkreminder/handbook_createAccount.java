93f051541e33167c535ef077a4723e386386f44f
skychoi1010 <skychoi1010@naver.com>
2020-05-28 20:20:46 +0900

    Firebase authentication - email implemented

--- app/src/main/java/com/waterdiary/drinkreminder/handbook_createAccount.java
+++ app/src/main/java/com/waterdiary/drinkreminder/handbook_createAccount.java
@@ -2,23 +2,55 @@ package com.waterdiary.drinkreminder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.waterdiary.drinkreminder.base.MasterBaseActivity;
 
 public class handbook_createAccount extends MasterBaseActivity {
+    FirebaseAuth mAuth;
+    EditText email;
+    EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.handbook_create_account);
         Button createAccount = findViewById(R.id.createacc);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        mAuth = FirebaseAuth.getInstance();
 
         createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), handbook_login.class);
-                startActivity(intent);
+                String email_str = email.getText().toString().trim();
+                String pwd_str = password.getText().toString().trim();
+
+                mAuth.createUserWithEmailAndPassword(email_str, pwd_str)
+                        .addOnCompleteListener(handbook_createAccount.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+                                    Intent intent = new Intent(handbook_createAccount.this, handbook_login.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(handbook_createAccount.this, "등록 에러", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            }
+                        });
             }
         });
     }
